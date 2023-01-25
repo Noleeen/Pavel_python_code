d6421ae8b8eb1919448431b5efa627588cc20f58
Ivan Gerasimenko <noleen@ya.ru>
2023-01-25 18:40:58 +0300

    hw1

---
+++ My_works/Work_1.py
@@ -0,0 +1,12 @@
+class WorkWithList:
+
+    def __init__(self):
+        self.array = []
+
+    def show(self):
+        for item in self.array:
+            print(item)
+
+
+li = WorkWithList()
+li.show()
